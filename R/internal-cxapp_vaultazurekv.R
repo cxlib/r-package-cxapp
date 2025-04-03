2e9981cd5f2e42ed6a1f476fd58a83dabb745c89
Magnus Mengelbier <magnus.mengelbier@gmail.com>
2025-04-03 08:05:57 +0000

    added method to list secrets in Azure Key Vault. Corrected pseudo-hierarchy to use dash instead of underscore

--- R/internal-cxapp_vaultazurekv.R
+++ R/internal-cxapp_vaultazurekv.R
@@ -1,19 +1,26 @@
 #' (Experimental) Internal reference class representing a connection to Azure Key Vault 
 #'
 #' @method initialize initialize
+#' @method list list
 #' @method secret secret
 #' @method show show
 #'
 #' @description
 #' An internal utility class representing a connection to an Azure Key Vault
 #' 
+#' The secret name consists of the characters a-z, 0-9 and dash (-).
+#' 
 #' The Azure Key Vault is not hierarchical but can use a path notation with 
 #' forward slash as separator to represent a crude hierarchy. The forward 
-#' slashes are translated to underscores mimicking a hierarchical reference 
+#' slashes are translated to dashes mimicking a hierarchical reference 
 #' structure.
 #' 
-#' The levels of the hierarchy and the secret name consists of the characters
-#' a-z, 0-9 and punctuation dash (-), underscore '_' and period '.'.
+#' The method `list()` lists the secrets.
+#' 
+#' The method `secret()` retrieves the secret from the Azure Key Vault. If the 
+#' secret does not exist or the connection returns other that success, the value
+#' of `unset` is returned.
+#' 
 #' 
 #' The Azure Key Vault relies on multiple cxapp properties.
 #' 
@@ -36,10 +43,6 @@
 #' 
 #' \strong{\emph{It is on the role of the developer and implementer to ensure 
 #' that the secrets stored in the vault and their use are appropriately protected.}}  
-#' 
-#' The `secret()` method retrieves a specified secret from the vault. If the
-#' secret is not defined or a connection failed, the value of `unset` is returned. 
-#' A secret is stored and returned as a single text value.
 #' 
 #'
 #' 
@@ -121,13 +124,71 @@
 
 
 
+.cxapp_vaultazurekv$methods( "list" = function() {
+  "List secrets"
+  
+  # -- initiate return
+  lst <- character(0)
+  
+
+  # -- get list of secrets from vault  
+  secret_url <- paste0( .self$.attr["url"], "/secrets", "?api-version=", .self$.attr["api-version"] )
+  
+  rslt_secret <- httr2::request( secret_url ) |>
+    httr2::req_method("GET") |>
+    httr2::req_auth_bearer_token( .self$.attr["token"] ) |>
+    httr2::req_perform()
+  
+  
+  if ( rslt_secret$status_code != 200 )
+    return(invisible(lst))
+  
+  
+  vault_secrets <- httr2::resp_body_json( rslt_secret )
+  
+  
+  # -- extract list of secrets
+  
+  if ( "value" %in% names(vault_secrets) )
+    for ( xidx in 1:length(vault_secrets[["value"]]) ) {
+      
+      # - expecting secret name to be part of id
+      if ( ! "id" %in% names(vault_secrets[["value"]][[xidx]]) )
+        next()
+      
+      
+      # - check if enabled attribute exists and if it is FALSE then do not include secret 
+      
+      if ( "attributes" %in% names(vault_secrets[["value"]][[xidx]]) &&
+           "enabled" %in% names(vault_secrets[["value"]][[xidx]][["attributes"]]) &&
+           ! vault_secrets[["value"]][[xidx]][["attributes"]][["enabled"]] )
+        next()
+      
+      # - append secret to list
+      lst <- append(lst, 
+                    gsub( paste0( "^", .self$.attr["url"],"/secrets/(.*)$"), "\\1", 
+                          vault_secrets[["value"]][[xidx]][["id"]], 
+                          perl = TRUE, ignore.case = TRUE ))
+      
+    }  
+  
+  
+  
+  # -- return
+  return(invisible(sort(lst)))
+  
+})
+  
+
+
+
 
 .cxapp_vaultazurekv$methods( "secret" = function( x, unset = NA ) {
   "Get secret"
   
   
   # -- convert to flat hierarchy 
-  xsecret <- base::gsub( "/", "_", x )
+  xsecret <- base::gsub( "/", "-", x )
   
   # -- remove leading underscore
   if ( base::startsWith( xsecret, "_" ) )
